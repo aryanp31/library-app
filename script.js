0a29b78d22f8a6958aeda50becae8ff06a26b548
Aryan Patel <aryanpatel31@yahoo.com>
2023-12-26 02:23:37 -0600

    Added basic CSS, added skeleton JS

---
+++ script.js
@@ -0,0 +1,14 @@
+class Book {
+  constructor(title="NA", author="NA", read=false) {
+    this.title = title;
+    this.author = author;
+    this.read = read;
+  }
+}
+
+let library = [];
+let newBook;
+
+function addBook() {
+  
+}
